526ff45ec676d8132d19c8ea1166126a76d4e3a1
CristiamMontoya4 <cristiam.montoya.avam@posbrava.io>
2024-08-27 11:23:11 -0500

    Logica para manejo de multiples menus

--- main.js
+++ main.js
@@ -2,17 +2,46 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const iconBurger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const iconCart = document.querySelector('.navbar-shopping-cart');
+const orderMenu = document.querySelector('.product-detail ');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconBurger.addEventListener('click', toggleMobileMenu);
+iconCart.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+
+    const isOpenOrderMenu = orderMenu.classList.contains('inactive');
+
+    if (!isOpenOrderMenu){
+        orderMenu.classList.add('inactive');
+    }
 
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    const isOpenOrderMenu = orderMenu.classList.contains('inactive');
+
+    if (!isOpenOrderMenu){
+        orderMenu.classList.add('inactive');
+    }
+
+}
+
+function toggleCartAside() {
+    orderMenu.classList.toggle('inactive');
+
+    const isOpenMobileMenu = mobileMenu.classList.contains('inactive');
+    const isOpenDesktopMenu = desktopMenu.classList.contains('inactive');
+
+    if (!isOpenMobileMenu){
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isOpenDesktopMenu){
+        desktopMenu.classList.add('inactive');
+    }
 
 }
